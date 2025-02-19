eeea1c3e6f76c208cf5a95d87ad00852baff2ce6
Hasitha Gamage <hasitha.hpmax@gmail.com>
2025-02-19 15:18:12 +0800

    feat: Add server initialization with UUID, health check, and basic Socket.IO setup

---
+++ server-v2/src/app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+const port = 3000
+const { v4: uuidv4 } = require("uuid")
+
+// Generate a UUID v4
+const uuid = uuidv4();
+
+const server_info = {
+    server_id: uuid
+}
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok' })
+});
+
+app.get('/set-cookie', (req, res) => {
+
+    const name = req.query.name
+    const value = req.query.value
+
+    res.cookie(name, value)
+    res.send({
+        "command": "set-cookie",
+        "cookies": {
+            [name]: value
+        }
+    })
+})
+
+app.get('/server-info', (req, res) => {
+    res.send(server_info)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
+
+
+io.on('connection', (socket) => {
+
+    setTimeout(_ => {
+        socket.emit("server-info", server_info)
+    }, 3000)
+    console.log("new connection", socket.id);
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
