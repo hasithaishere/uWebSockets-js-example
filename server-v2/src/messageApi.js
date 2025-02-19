d6855a33c472e52f3cb3f9e40ad44782f299c999
Hasitha Gamage <hasitha.hpmax@gmail.com>
2025-02-19 14:48:26 +0800

    feat: Define constant for CALB cookie name

--- server-v2/src/messageApi.js
+++ server-v2/src/messageApi.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 
+const CALB_COOKIE = 'CALBCOOK';
+
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
